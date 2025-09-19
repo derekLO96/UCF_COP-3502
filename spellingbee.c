b769ada120bd34b61cf96256b1f863ba8fa4c988
DerekO <dloyobro@gmail.com>
2025-09-19 07:43:23 -0400

    worked more on the p2 project, about 80% done. need to find out where to fit in binary search.

--- spellingbee.c
+++ spellingbee.c
@@ -18,8 +18,11 @@ typedef struct validWordsStruct
 
 spellingbee* BuildSpellingBeeStruct(char* spellingbeeLetters);
 validWordsStruct* PlayGame(int wordCount , spellingbee* spellingbeeStructPointer);
-validWordsStruct* CheckWordValidity( validWordsStruct* validWordStruct , char* wordToBeChecked , spellingbee* spellingbeeStructPointer);
-validWordsStruct* ConstructValidWordsStruct(char* wordToBeChecked);
+char* SortAlphabetical(char* wordToBeSorted);
+validWordsStruct* AddToLinkedList( validWordsStruct* validWordStruct , char* wordToBeChecked , spellingbee* spellingbeeStructPointer);
+validWordsStruct* NodeFactory(char* wordToBeChecked);
+validWordsStruct* CheckWordValidity(validWordsStruct* validWords, spellingbee* spellingbeeStructPointer);
+int CheckForSpecialCharacter(validWordsStruct* temporary , char specialCharacter);
 void printLinkedList(validWordsStruct* validWords);
 
 int main()
@@ -27,12 +30,15 @@ int main()
     int wordCount = 0;
     char* spellingbeeLetters = malloc(51 * sizeof(char));
     scanf("%s" , spellingbeeLetters);
-    spellingbee* spellingbeeStructPointer = BuildSpellingBeeStruct(spellingbeeLetters);
+    spellingbeeLetters = SortAlphabetical(spellingbeeLetters);
+
+    spellingbee* spellingbeeStructPointer = BuildSpellingBeeStruct(spellingbeeLetters);    
 
     scanf("%d" , &wordCount);
     validWordsStruct* validWords = PlayGame(wordCount , spellingbeeStructPointer);
 
-    printLinkedList(validWords);
+
+    printLinkedList(CheckWordValidity(validWords, spellingbeeStructPointer));
 
 
     printf("\n");
@@ -41,40 +47,134 @@ int main()
     return 0;
 
 }
-void printLinkedList(validWordsStruct* validWords)
-{
-    while(validWords->next != NULL)
-    {
-        printf("%s\t" , validWords->validWord);
 
-        validWords = validWords->next;
-    }
+//TO DO: figure out where this goes
+//binary search algorithm from class to find the words?
+int binsearch(char** words, int begginingOfSearch, int endOfSearch, char* target) {
+
+    // Empty search space.
+    if (begginingOfSearch > endOfSearch) return -1;
+
+    int middleOfSearch = (begginingOfSearch+endOfSearch)/2;
+
+    // Save this since we need it twice.
+    int cmp = strcmp(target, words[middleOfSearch]);
+
+    // target must be in left side of array.
+    if (cmp < 0)
+        return binsearch(words, begginingOfSearch, middleOfSearch-1, target);
+
+    // right side...
+    else if (cmp > 0)
+        return binsearch(words, middleOfSearch+1, endOfSearch, target);
+
+    // We found it!
+    else
+        return middleOfSearch;
 }
 
+//helper function that uses recursion to print out each node in our linked list.
+void printLinkedList(validWordsStruct* validWordsStructPointer)
+{
+        if(!validWordsStructPointer) return;
+        printf("\t%s\n" , validWordsStructPointer->validWord);
+        printLinkedList(validWordsStructPointer->next);
+
+        
+}
+validWordsStruct* CheckWordValidity(validWordsStruct* validWords, spellingbee* spellingBeeStruct)
+{
+    validWordsStruct* validWordStructLinkedList;
+
+    validWordsStruct* head = validWords;
+    while(validWords->next != NULL)
+    {
+        validWordsStruct* temporary = validWords;
+
+        if(CheckForSpecialCharacter(temporary , spellingBeeStruct->lettersInGame[3]))
+        {
+
+        }
+        else
+        {
+            
+
+        }
+
+    }
+
+
+    return validWordStructLinkedList;
+}
+
+//sorts a string into alphabetical order.
+char* SortAlphabetical(char* wordToBeSorted)
+{
+    //store the string length
+    int wordLength = strlen(wordToBeSorted);
+
+    //loop through the word keeping two indexes, we compare the first letter to all other
+    //letters in the word and see which one has the lowest value, indicating it comes first in the alphabet
+    //then we step through to the other letters until we reach the end of the word.
+    for(int i = 0 ; i < wordLength ; i++)
+    {
+        for(int j = (i + 1) ; j < wordLength ; j++)
+        {
+            char temporaryCharacter = wordToBeSorted[i];
+            if(wordToBeSorted[j] < temporaryCharacter)
+            {
+                wordToBeSorted[i] = wordToBeSorted[j];
+                wordToBeSorted[j] = temporaryCharacter;
+            }
+        }
+
+    }
+
+    return wordToBeSorted;
+
+}
+
+
+//simple helper function that returns 1(True) if the special spellingbee character is in the string and 0(if its not)
+int CheckForSpecialCharacter(validWordsStruct* temporary , char specialCharacter)
+{
+    for(int i = 0 ; i < strlen(temporary->validWord) ; i++)
+    {
+        if(temporary->validWord[i] == specialCharacter) return 1;
+
+    }
+
+    return 0;
+}
+
+//takes in a number of how many words in the game and the struct to the pellingbee characters(already sorted) to pass to a 
+//helper function. 
+//this function is just to return the already built final answer to our problem.
 validWordsStruct* PlayGame(int wordCount , spellingbee* spellingbeeStructPointer)
 {
+    validWordsStruct* validWordsLinkedList = NULL;
+
     char* wordToBeChecked = malloc(51 * sizeof(char));
-    validWordsStruct* validWords = malloc(sizeof(validWordsStruct));
-    validWords->validWord = malloc(51 * sizeof(char));
 
     if(wordCount < 1) return NULL;
 
     while(wordCount > 0)
     {
-        char* wordToBeChecked = malloc(51 * sizeof(char));
         scanf("%s" , wordToBeChecked);
 
-        validWords = CheckWordValidity(validWords , wordToBeChecked , spellingbeeStructPointer);
+        validWordsLinkedList = AddToLinkedList(validWordsLinkedList , wordToBeChecked , spellingbeeStructPointer);
 
         wordCount--;
 
+        
     }
 
     free(wordToBeChecked);
-    return validWords;
+    return validWordsLinkedList;
 
 }
 
+//simple helper funtion that returns a built struct
 spellingbee* BuildSpellingBeeStruct(char* spellingbeeLetters)
 {
     spellingbee* spellingbeeStructPointer = malloc(sizeof(spellingbee));
@@ -87,47 +187,41 @@ spellingbee* BuildSpellingBeeStruct(char* spellingbeeLetters)
     return spellingbeeStructPointer;
 }
 
-validWordsStruct* ConstructValidWordsStruct(char* wordToBeChecked)
+//builds and returns a new node to the linked list
+validWordsStruct* NodeFactory(char* wordToBeChecked)
 {
-    validWordsStruct* temporary = malloc(sizeof(validWordsStruct));
-        temporary->validWord = malloc(strlen(wordToBeChecked) + 1);
-        strcpy(temporary->validWord , wordToBeChecked);
-        temporary->next = NULL;
+    validWordsStruct* newNode = malloc(sizeof(validWordsStruct));
+        newNode->validWord = malloc(strlen(wordToBeChecked) + 1);
+        strcpy(newNode->validWord , wordToBeChecked);
+        newNode->next = NULL;
 
-    return temporary;
+    return newNode;
 
 }
 
-validWordsStruct* CheckWordValidity(validWordsStruct* validWordStructPointer , char* wordToBeChecked, spellingbee* spellingbeeStructPointer)
+
+//takes in some parameters to append to the end of the linked list, maintaining the original order of 
+//the input to the program(already in alphabetical order!)
+validWordsStruct* AddToLinkedList(validWordsStruct* validWordStructPointer , char* wordToBeChecked, spellingbee* spellingbeeStructPointer)
 {
+        validWordsStruct* newNode = NodeFactory(wordToBeChecked);
 
-    /*
-        int wordLength = strlen(wordToBeChecked);
-        
-        int found = 0;
+        if(validWordStructPointer == NULL){
+             return newNode;
+            }
 
-        for(int i = 0 ; i < wordLength ; i++)
+        validWordsStruct* headNode = validWordStructPointer;
+
+        while(validWordStructPointer->next != NULL)
         {
-
-            if(wordToBeChecked[i] == spellingbeeStructPointer->requiredLetter) found = 1;
-            
+            validWordStructPointer = validWordStructPointer->next;
 
         }
 
-        if(found == 0) return;*/
+        validWordStructPointer->next = newNode;
 
-        validWordsStruct* temporary = ConstructValidWordsStruct(wordToBeChecked);
-
-        if(validWordStructPointer == NULL)
-        {
-            validWordStructPointer = temporary;
-        }
-        else
-        {
-            temporary->next = validWordStructPointer;
-        }
-
-        return temporary;
+        return headNode;
 
 }
+
 
